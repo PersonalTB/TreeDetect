a6410ddbf4dd8b9a7a0578dd2dcaa95bb6b6ae28
PersonalTB <141638456+PersonalTB@users.noreply.github.com>
2023-09-12 18:19:53 +0200

    Delete TreeDetect/analyses/tree_graph_analysis.py

--- TreeDetect/analyses/tree_graph_analysis.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from TreeDetect.data_preparation.preprocess import (create_delaunay_graph_from_geographic_points)
-
-def graph_analysis(csv_filepath, settings):
-	points = pd.read_csv(csv_filepath)
-	lon_lat_coords = points[['longitude','latitude']].to_numpy()
